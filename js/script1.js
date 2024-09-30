ab6c361752c10979b2307b6ae370b2a689a07204
ItalowBrabo <149082990+ItalowBrabo@users.noreply.github.com>
2024-09-30 09:58:32 -0300

    Add files via upload

---
+++ js/script1.js
@@ -0,0 +1,31 @@
+function codigoBRW(codigo){
+   navigator.clipboard.writeText(codigo).then (() => {
+        alert('Código copiado, paga ai pa noiz 😁');
+   }).catch(err => {
+        console.error('Erro ao copiar código: ', err);
+   });
+
+}
+function codigoLeo(codigo){
+    navigator.clipboard.writeText(codigo).then (() =>{
+        alert('Código copiado, paga ai pa noiz 😁');
+    }).catch(err =>{
+        console.error('Erro ao copiar código: ',err);
+    });
+}
+
+function codigoKaz(codigo){
+    navigator.clipboard.writeText(codigo).then (() => {
+        alert('Código copiado, paga ai pa noiz 😁');
+    }).catch(err =>{
+        console.error('Erro ao copiar código',err);
+    });
+}
+
+function codigoFan(codigo){
+    navigator.clipboard.writeText(codigo).then (() => {
+        alert('Código copiado, paga ai pa noiz 😁');
+    }).catch(err =>{
+        console.error('Erro ao copiar código',err);
+    });
+}
